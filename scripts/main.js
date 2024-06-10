4b3f892879de3f30284fd9aba9bfca1f7189afb4
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-06-10 17:15:50 +0200

    add: javascript code file

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// Scripts
+
+/* sticky navbar */
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("sticky");
+  } else {
+    document.getElementById("navbar").classList.remove("sticky");
+  }
+}
+
+/* Active links */
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
